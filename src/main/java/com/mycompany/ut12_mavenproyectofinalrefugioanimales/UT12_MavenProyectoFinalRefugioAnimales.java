631cde4d53e2f0a394f70042d464bcd568f174c8
Imanecm <chaibiimen4@gmail.com>
2025-04-08 09:56:12 +0200

    PruebaInicial

---
+++ src/main/java/com/mycompany/ut12_mavenproyectofinalrefugioanimales/UT12_MavenProyectoFinalRefugioAnimales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ut12_mavenproyectofinalrefugioanimales;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class UT12_MavenProyectoFinalRefugioAnimales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
